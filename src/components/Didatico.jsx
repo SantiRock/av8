7bb8c0fe5c23d1d1d382e522e0e5b692f8a29e4d
SantiRock <sqp.dev@gmail.com>
2026-08-17 12:43:31 -0300

    pdfs

--- src/components/Didatico.jsx
+++ src/components/Didatico.jsx
@@ -6,12 +6,21 @@ export const Didatico = () => {
     return (
         <section>
             <h2>Material Didático</h2>
+            <p>PDFs</p>
+            <ul>
+                <li><a href="/Introducao_aos_Shaders_Pt.pdf" target="_blank">Introdução_aos_Shaders_Pt.pdf</a></li>
+                <li><a href="/Introduccion_a_los_shaders_Es.pdf" target="_blank">Introducción_a_los_Shaders_Es.pdf</a></li>
+            </ul>
+            <p>Webs</p>
             <ul>
                 <li><a href="https://santirockk.github.io/fsexample1/" target="_blank">Fragment Shader Example 1</a></li>
                 <li><a href="https://santirockk.github.io/fspoints/" target="_blank">Fragment Shader Example 2</a></li>
                 <li><a href="https://santirockk.github.io/webgl_drawing_modes/" target="_blank">WebGL Drawing Modes</a></li>
                 <li><a href="https://santirockk.github.io/test-skull/" target="_blank">Skull 3D</a></li>
             </ul>
+            <div className="of_c1">
+                <a className="med" href="/Santiago_Quintero_Mediador_Linguistico_Artes.pdf" target="_blank" rel="noopener noreferrer"><span className="med_s">Mediação Linguística e Comunicação Intercultural para Artes e Cultura / </span>Link</a>
+            </div>
         </section>
     )
 }
--- src/components/Oficinas.jsx
+++ src/components/Oficinas.jsx
@@ -7,7 +7,7 @@ export const Oficinas = () => {
         <section>
             <h2>Oficinas</h2>
             <div className="of_c">
-                <a className="o_l" href="https://oficina-shaders.vercel.app/" target="_blank"><span className="o_p">- Shaders ao Vivo, introdução para artistas visuais e VJs / </span>Link</a>
+                <a className="o_l" href="https://oficina-shaders.vercel.app/" target="_blank"><span className="o_p">- Shaders ao Vivo, introdução / </span>Link</a>
                 <p>- Shaders: programação, escrevendo shaders</p>
                 <p>- Resolume</p>
                 <p>- openFrameworks</p>
@@ -17,10 +17,6 @@ export const Oficinas = () => {
                 <p>Fique à vontade para entrar em contato.</p>
                 <a href="https://www.instagram.com/av8visual/" target="_blank">@av8visual</a>
             </div>
-            <div className="of_c1">
-                <a className="med" href="/Santiago_Quintero_Mediador_Linguistico_Artes.pdf" target="_blank" rel="noopener noreferrer"><span className="med_s">Mediação Linguística e Comunicação Intercultural para Artes e Cultura / </span>Link</a>
-            </div>
-
         </section>
     )
 }
